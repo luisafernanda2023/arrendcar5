0e694afeaa564fafc00ba1b066de964a42ed6b9d
John Doe <johndoe@example.com>
2020-04-21 23:28:16 -0500

    first commit

---
+++ ClientApp/src/app/vehiculo/vehiculoAdd/vehiculo-add/vehiculo-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehiculo-add',
+  templateUrl: './vehiculo-add.component.html',
+  styleUrls: ['./vehiculo-add.component.css']
+})
+export class VehiculoAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# arrendacar"  
+"# arrendacar2"  
